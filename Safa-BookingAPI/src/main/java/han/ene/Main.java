c85bb3d4e7425cb07ffdbea07cf721590e6a1003
SafaT06 <139579633+SafaT06@users.noreply.github.com>
2025-03-18 11:37:35 +0100

    les 2af en voorbereiding toegevoegd (moet nog maken)

--- Safa-BookingAPI/src/main/java/han/ene/Main.java
+++ Safa-BookingAPI/src/main/java/han/ene/Main.java
@@ -6,12 +6,17 @@ import kong.unirest.Unirest;
 public class Main {
 
   public static void main(String[] args) {
-    HttpResponse<String> response = Unirest.get("https://booking-com15.p.rapidapi.com/api/v1/hotels/searchDestination?query=man")
-        .header("X-RapidAPI-Host", "booking-com15.p.rapidapi.com")
-        .header("X-RapidAPI-Key", "cdce59bb45msh63300f130fa230cp1e5fcejsn83361f598203")
+    String host = " X-RapidAPI-Host";
+    String key = " X-RapidAPI-Key";
+
+    HttpResponse<String> response = Unirest.get(
+            "https://booking-com15.p.rapidapi.com/api/v1/hotels/searchDestination?query=man")
+        .header(host, "booking-com15.p.rapidapi.com")
+        .header(key, "cdce59bb45msh63300f130fa230cp1e5fcejsn83361f598203")
         .asString();
 
-    System.out.println("Status code: " + response.getStatus());
+    int status = response.getStatus();
+    System.out.println("Status code: " + status);
 
     String responseBody = response.getBody();
     System.out.println("Response body: " + responseBody);
