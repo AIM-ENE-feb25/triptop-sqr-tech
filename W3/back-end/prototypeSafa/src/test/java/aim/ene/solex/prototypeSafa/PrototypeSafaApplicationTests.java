31b8faafa65955ac41669e6b6c791e3604dfa815
SafaT06 <139579633+SafaT06@users.noreply.github.com>
2025-04-01 10:32:35 +0200

    back-end toegevoegd Safa

---
+++ W3/back-end/prototypeSafa/src/test/java/aim/ene/solex/prototypeSafa/PrototypeSafaApplicationTests.java
@@ -0,0 +1,13 @@
+package aim.ene.solex.prototypeSafa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrototypeSafaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
